84e908e8abc8dedbceb42f0c943958fe887b85e6
Jian-Qing Zheng <jianqingzheng@users.noreply.github.com>
2024-03-10 20:32:27 +0000

    2

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -108,3 +108,22 @@ $(document).ready(function(){
   //   var fold_content = document.getElementById("fold_content");
 
 });
+
+
+
+function toggleDropdown() {
+  document.getElementById("dropdownLinks").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropdown-button')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}

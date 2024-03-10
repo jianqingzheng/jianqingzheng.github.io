59bbd0baa6600e007f18e46901562ba8d592db0c
Jian-Qing Zheng <jianqingzheng@users.noreply.github.com>
2024-03-10 20:56:04 +0000

    3

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -109,21 +109,3 @@ $(document).ready(function(){
 
 });
 
-
-
-function toggleDropdown() {
-  document.getElementById("dropdownLinks").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches('.dropdown-button')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (var i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
